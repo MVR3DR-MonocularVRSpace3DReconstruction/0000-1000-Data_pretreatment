1ed60a08483ffcb71160c4c27fe47218bf78e46c
our0boros <863166709@qq.com>
2022-10-21 18:28:57 +0800

    failed detect

--- processVideo.py
+++ processVideo.py
@@ -27,29 +27,34 @@ keyPressed = False
 videos = sorted(glob.glob(raw_dir+"*.h264"))
 
 for v in videos:
-    print("=> Processing Data...")
-    print("=> ", v)
-    out = v.split("/")[-1].replace(".h264", "")
-    print("=> ",out)
-    cmd = 'ffmpeg -i {} -filter:v "crop=in_w/2:in_h:0:0" -c:a copy {}-right.mp4'.format(v, out_dir+out)
-    subprocess.run(cmd, shell=True)
+    try:     
+        print("=> Processing Data...")
+        print("=> ", v)
+        out = v.split("/")[-1].replace(".h264", "")
+        print("=> ",out)
+        cmd = 'ffmpeg -i {} -filter:v "crop=in_w/2:in_h:0:0" -c:a copy {}-right.mp4'.format(v, out_dir+out)
+        subprocess.run(cmd, shell=True)
 
-    cmd = 'ffmpeg -i {} -filter:v "crop=in_w/2:in_h:in_w/2:0" -c:a copy {}-left.mp4'.format(v, out_dir+out)
-    subprocess.run(cmd, shell=True)
+        cmd = 'ffmpeg -i {} -filter:v "crop=in_w/2:in_h:in_w/2:0" -c:a copy {}-left.mp4'.format(v, out_dir+out)
+        subprocess.run(cmd, shell=True)
 
-    print("=> L/R sights saved.")
+        print("=> L/R sights saved.")
 
-    print("=> Slice frames")
+        print("=> Slice frames")
 
-    os.system("rm -rf {0}/{1} && mkdir {0}/{1}".format(frame_dir, out))
-    os.system("mkdir {0}/{1}/left && mkdir {0}/{1}/right".format(frame_dir, out))
+        os.system("rm -rf {0}/{1} && mkdir {0}/{1}".format(frame_dir, out))
+        os.system("mkdir {0}/{1}/left && mkdir {0}/{1}/right".format(frame_dir, out))
 
-    cmd = "ffmpeg -i {}-left.mp4 {}/{}/left/%05d.png".format(out_dir+out, frame_dir, out)
-    subprocess.run(cmd, shell=True)
-    cmd = "ffmpeg -i {}-right.mp4 {}/{}/right/%05d.png".format(out_dir+out, frame_dir, out)
-    subprocess.run(cmd, shell=True)
-
-    os.system("mv {0} {0}.fin".format(v))
-    print("=> Frames sliced.")
-
-
+        cmd = "ffmpeg -i {}-left.mp4 {}/{}/left/%05d.png".format(out_dir+out, frame_dir, out)
+        subprocess.run(cmd, shell=True)
+        cmd = "ffmpeg -i {}-right.mp4 {}/{}/right/%05d.png".format(out_dir+out, frame_dir, out)
+        subprocess.run(cmd, shell=True)
+        
+    except Exception as e:  
+        # 其他例外發生的時候，所要做的動作
+        print("Other error or exception occurred!" )
+        print(e)
+    
+    finally:  
+        os.system("mv {0} {0}.fin".format(v))
+        print("=> Frames sliced.")
