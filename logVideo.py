aba13ae2d08c909cfee83be6528ad27d354c5d7a
our0boros <863166709@qq.com>
2022-10-21 17:25:51 +0800

    conflic

--- logVideo.py
+++ logVideo.py
@@ -90,9 +90,10 @@ except KeyboardInterrupt:
     # 當你按下 CTRL+C 中止程式後，所要做的動作
     print("STOP")
   
-except:  
+except Exception as e:  
     # 其他例外發生的時候，所要做的動作
     print("Other error or exception occurred!" )
+    print(e)
   
 finally:  
     GPIO.cleanup() # 把這段程式碼放在 finally 區域，確保程式中止時能夠執行並清掉GPIO的設定！
