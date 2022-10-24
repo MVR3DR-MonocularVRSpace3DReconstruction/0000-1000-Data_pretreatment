6facaddf16a193a2faad588c5b97282f3daa4909
our0boros <863166709@qq.com>
2022-10-24 15:46:10 +0800

    use raspivid

--- logVideo.py
+++ logVideo.py
@@ -20,27 +20,29 @@ frame_dir = "frames/"
 GPIO.setmode(GPIO.BCM)
 recording_key = 5
 GPIO.setup(recording_key,GPIO.IN,GPIO.PUD_UP)
+useRaspiCam = False
 
 ####################################################
 # Camera init
 ####################################################
-cam = picamera.PiCamera(stereo_mode='side-by-side', stereo_decimate=False)
-cam.framerate = 25
-cam.resolution = (1280, 480) # 1280 x 480   2560 x 720  960 x 320
-# cam.exposure_mode = 'antishake' # auto
+if useRaspiCam:
+    cam = picamera.PiCamera(stereo_mode='side-by-side', stereo_decimate=False)
+    cam.framerate = 25
+    cam.resolution = (1280, 480) # 1280 x 480   2560 x 720  960 x 320
+    # cam.exposure_mode = 'antishake' # auto
 
-# cam.saturation = 80 # 设置图像视频的饱和度
-# cam.brightness = 50 # 设置图像的亮度(50表示白平衡的状态)
-# cam.shutter_speed = 6000000 # 相机快门速度
-# cam.iso = 800 
-# ISO标准实际上就是来自胶片工业的标准称谓，ISO是衡量胶片对光线敏感程度的标准。
-# 如50 ISO, 64 ISO, 100 ISO表示在曝光感应速度上要比高数值的来得慢，高数值ISO是指超过200以上的标准，如200 ISO, 400 ISO
-cam.sharpness = 0 #设置图像的锐度值，默认是0，取值范围是-100~100之间
+    # cam.saturation = 80 # 设置图像视频的饱和度
+    # cam.brightness = 50 # 设置图像的亮度(50表示白平衡的状态)
+    # cam.shutter_speed = 6000000 # 相机快门速度
+    # cam.iso = 800 
+    # ISO标准实际上就是来自胶片工业的标准称谓，ISO是衡量胶片对光线敏感程度的标准。
+    # 如50 ISO, 64 ISO, 100 ISO表示在曝光感应速度上要比高数值的来得慢，高数值ISO是指超过200以上的标准，如200 ISO, 400 ISO
+    cam.sharpness = 0 #设置图像的锐度值，默认是0，取值范围是-100~100之间
 
-cam.vflip = True
-cam.hflip = True
-cam.start_preview(fullscreen=False, window=(0,0,640,480))
-# cam.start_preview()
+    cam.vflip = True
+    cam.hflip = True
+    cam.start_preview(fullscreen=False, window=(0,0,640,480))
+    # cam.start_preview()
 
 ####################################################
 # Start
@@ -63,7 +65,7 @@ try:
             if GPIO.input(recording_key) == 0:
                 keyPressed = True
                 while GPIO.input(recording_key) == 0:
-                    if recording:
+                    if recording and useRaspiCam:
                         cam.wait_recording(0.01)
                     else:
                         time.sleep(0.01)
@@ -72,18 +74,35 @@ try:
         ####################################################
         # Recording 
         ####################################################
-        if not recording and keyPressed:
-            print("=> Start Recording")
-            videoTime = datetime.now().strftime("%y-%m-%d_%H-%M-%S")
-            in_route = raw_dir + videoTime + ".h264"
-            cam.start_recording(in_route, format="h264", quality=10)
-            full_light()
-            recording = not recording
-        elif recording and keyPressed:
-            cam.stop_recording()
-            print("=> Done! <{}> saved!".format(videoTime))
-            full_dark()
-            recording = not recording
+        if useRaspiCam:
+            if not recording and keyPressed:
+                print("=> Start Recording")
+                videoTime = datetime.now().strftime("%y-%m-%d_%H-%M-%S")
+                in_route = raw_dir + videoTime + ".h264"
+                cam.start_recording(in_route, format="h264", quality=10)
+                full_light()
+                recording = not recording
+            elif recording and keyPressed:
+                cam.stop_recording()
+                print("=> Done! <{}> saved!".format(videoTime))
+                full_dark()
+                recording = not recording
+        else:
+            if not recording and keyPressed:
+                print("=> Start Recording")
+                videoTime = datetime.now().strftime("%y-%m-%d_%H-%M-%S")
+                # in_route = raw_dir + videoTime + ".h264"
+                # cam.start_recording(in_route, format="h264", quality=10)
+                os.system("raspivid -w 1280 -h 720 -p 0,0,960,540 -cs 0 -t 20000 -o camera0.h264 & raspivid -w 1280 -h 720 -p 960,0,960,540 -cs 1 -t 20000 -o camera1.h264")
+                full_light()
+                recording = not recording
+            elif recording and keyPressed:
+                # cam.stop_recording()
+                os.exit()
+                print("=> Done! <{}> saved!".format(videoTime))
+                full_dark()
+                recording = not recording
+
 
         keyPressed = False
     
